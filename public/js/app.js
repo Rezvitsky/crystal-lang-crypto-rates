a0c774384ddab98788c69e1f8f6a2d4491891649
Leonid Rezvitsky <rezvitsky@swert.ru>
2022-04-23 14:10:24 +0700

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,31 @@
 Vue.createApp({
     data() {
         return {
+            loading: true,
+            error: '',
             query: '',
-            tokens: [
-                {
-                    name: 'Bitcoin',
-                    symbol: 'BTC',
-                    logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.svg',
-                    usd: 0
-                },
-                {
-                    name: 'Ethereum',
-                    symbol: 'ETH',
-                    logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.svg',
-                    usd: 0
-                },
-                {
-                    name: 'Binance coin',
-                    symbol: 'BNB',
-                    logo: 'https://cryptologos.cc/logos/bnb-bnb-logo.svg',
-                    usd: 0
-                },
-                {
-                    name: 'Tether',
-                    symbol: 'USDT',
-                    logo: 'https://cryptologos.cc/logos/tether-usdt-logo.svg',
-                    usd: 0
-                },
-                {
-                    name: 'Tron',
-                    symbol: 'TRX',
-                    logo: 'https://cryptologos.cc/logos/tron-trx-logo.svg',
-                    usd: 0
-                },
-                {
-                    name: 'Binance usd',
-                    symbol: 'BUSD',
-                    logo: 'https://cryptologos.cc/logos/binance-usd-busd-logo.svg',
-                    usd: 0
-                },
-                {
-                    name: 'Cardano',
-                    symbol: 'ADA',
-                    logo: 'https://cryptologos.cc/logos/cardano-ada-logo.svg',
-                    usd: 0
-                }
-            ]
+            tokens: []
         }
     },
 
     async mounted() {
-        const result = await fetch('/rates?ids=bitcoin,ethereum,binancecoin,tether,tron,busd,cardano')
+        this.find()
+    },
 
-        const response = await result.json()
-
-        this.tokens[0].usd = response?.bitcoin.usd
-        this.tokens[1].usd = response?.ethereum.usd
-        this.tokens[2].usd = response?.binancecoin.usd
-        this.tokens[3].usd = response?.tether.usd
-        this.tokens[4].usd = response?.tron.usd
-        this.tokens[5].usd = response?.busd.usd
-        this.tokens[6].usd = response?.cardano.usd
+    methods: {
+        async find(query = undefined) {
+            try {
+                const result = await fetch(`/rates?query=${ query }`)
+    
+                const response = await result.json()
+    
+                this.tokens = response
+    
+                this.loading = false
+            } catch (error) {
+                this.loading = false
+                this.error = error
+            }
+        }
     }
 }).mount('#app')
--- src/crystal-lang-crypto-rates.cr
+++ src/crystal-lang-crypto-rates.cr
@@ -6,13 +6,58 @@ end
 
 get "/rates" do |env|
   env.response.content_type = "application/json"
-  ids = env.params.query["ids"]
+  query = env.params.query["query"]
+    
+  response = HTTP::Client.get "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,tether,tron,busd,cardano&vs_currencies=usd&include_24hr_change=true"
 
-  HTTP::Client.get("https://api.coingecko.com/api/v3/simple/price?ids=#{ ids }&vs_currencies=usd&include_24hr_change=true") do |response|
-    response.status_code
-    response.body_io.gets
-  end
+  result = JSON.parse(response.body.lines.first)
+
+  puts query
+
+  tokens = [
+    {
+      name: "Bitcoin",
+      symbol: "BTC",
+      logo: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg",
+      usd: result["bitcoin"]["usd"]
+    },
+    {
+      name: "Ethereum",
+      symbol: "ETH",
+      logo: "https://cryptologos.cc/logos/ethereum-eth-logo.svg",
+      usd: result["ethereum"]["usd"]
+    },
+    {
+      name: "Binance coin",
+      symbol: "BNB",
+      logo: "https://cryptologos.cc/logos/bnb-bnb-logo.svg",
+      usd: result["binancecoin"]["usd"]
+    },
+    {
+      name: "Tether",
+      symbol: "USDT",
+      logo: "https://cryptologos.cc/logos/tether-usdt-logo.svg",
+      usd: result["tether"]["usd"]
+    },
+    {
+      name: "Tron",
+      symbol: "TRX",
+      logo: "https://cryptologos.cc/logos/tron-trx-logo.svg",
+      usd: result["tron"]["usd"]
+    },
+    {
+      name: "Binance usd",
+      symbol: "BUSD",
+      logo: "https://cryptologos.cc/logos/binance-usd-busd-logo.svg",
+      usd: result["busd"]["usd"]
+    },
+    {
+      name: "Cardano",
+      symbol: "ADA",
+      logo: "https://cryptologos.cc/logos/cardano-ada-logo.svg",
+      usd: result["cardano"]["usd"]
+    }
+  ].to_json
 end
 
-
-Kemal.run
+Kemal.run(3021)
